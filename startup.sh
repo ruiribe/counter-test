7563cbcbacd34cb3b5bd4c96219149e5832bedb0
ruiribe <ruiribe@gmail.com>
2017-07-20 12:09:25 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://github.com/ruiribe/counter-test
-cd /counter-test
+cd counter-test
 #sh install_docker.sh
 #sh install_jdk_maven.sh
 mvn clean install -f ./counter-test
-docker-compose up -d nginx
+docker-compose up -d
 docker-compose logs -f
